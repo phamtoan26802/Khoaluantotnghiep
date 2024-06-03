acdb208bbeae757c3404c03572f69333968f389a
phamtoan26802 <emcodi123@gmail.com>
2024-06-03 20:58:09 +0700

    project

---
+++ Frontend/src/page/Contact.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Contact = () => {
+    return (
+        <div className='h-full flex flex-col items-center justify-center'>
+            <div className='w-1/2 flex flex-col gap-10 p-10 shadow-md rounded-xl bg-white'>
+                <div className='font-bold text-xl'>Thông tin liên hệ</div>
+                <div><strong>Địa chỉ: </strong>P.1505 - A2 Tòa nhà Ecolife Capitol - 58 Tố Hữu, Thanh Xuân, Hà Nội</div>
+                <div><strong>Phone: </strong>02438484748 - 02438373633</div>
+
+                <div><strong>Email: </strong>leskin@gmail.com</div>
+
+                <div><strong>Website: </strong>leskin.com</div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Contact
